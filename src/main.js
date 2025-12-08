0d3dfb15cd22650bb22863e45cfa871792aadc97
unknown <ryotas@uchicago.edu>
2025-12-08 05:37:05 -0600

    updated

--- src/main.js
+++ src/main.js
@@ -22,30 +22,55 @@ main();
 function selectingtop(selectedData, topN){
 
     const totalExport = {};
-    selectedData.forEach(d => {
+    for (const d of selectedData){
         totalExport[d.source] = (totalExport[d.source] || 0) + d.value;
-    });
+    };
     
     const totalImport = {};
-    selectedData.forEach(d => {
+    for (const d of selectedData){
         totalImport[d.target] = (totalImport[d.target] || 0) + d.value
-    });
+    };
 
-    const topExport = new Set(
-        Object.entries(totalExport)
+    const topExport = Object.entries(totalExport)
         .sort((a, b) => b[1] - a[1])
         .slice(0, topN)
-        .map(d => d[0])
-    );
+        .map(d => d[0]);
 
-    const topImport = new Set(
-        Object.entries(totalImport)
+    const topImport = Object.entries(totalImport)
         .sort((a, b) => b[1] - a[1])
         .slice(0, topN)
-        .map(d => d[0])
-    );
+        .map(d => d[0]);
     
     return [topExport, topImport]
+}
+
+function createLinksandNodes(selectedData, excountry, imcountry){
+    const selectedLinks = selectedData.map(d => {
+        const source = excountry.includes(d.source) ? d.source:"Others";
+        const target = imcountry.includes(d.target) ? d.target:"Others";
+        return {source:source + "(Ex)", target:target + "(Im)", value:d.value}
+    })
+
+    const integratedLinks = {};
+    for (const l of selectedLinks){
+        const key = l.source + "%" + l.target;
+        integratedLinks[key] = (integratedLinks[key] || 0) + l.value;
+    };
+
+    const simpleLinks = Object.entries(integratedLinks).map(([key, value]) => {
+        const [source, target] = key.split("%");
+        return {source:source, target:target, value:value };
+        });
+
+    const nodeIds = new Set();
+    for (const l of simpleLinks){
+        nodeIds.add(l.source);
+        nodeIds.add(l.target);
+        };
+    const selectedNodes = Array.from(nodeIds).map(d => ({id: d}));
+
+    return {links:simpleLinks, nodes:selectedNodes};
+
 }
 
 function updateChart() {
@@ -57,50 +82,27 @@ function updateChart() {
 
     const [topExport, topImport] = selectingtop(selected, topN);
 
-    const selectedLinks = selected.map(d => {
-        const source = topExport.has(d.source) ? d.source:"Others";
-        const target = topImport.has(d.target) ? d.target:"Others";
-        return {source:source + "(Ex)", target:target + "(Im)", value:d.value}
-    })
+    const {links, nodes} = createLinksandNodes(selected, topExport, topImport)
 
-    const integratedLinks = {};
-    selectedLinks.forEach(d => {
-        const key = d.source + "%" + d.target;
-        integratedLinks[key] = (integratedLinks[key] || 0) + d.value;
-    });
+    let orderList = topExport.map(d => d+"(Ex)")
+        .concat(topImport.map(d => d + "(Im)"));
+    orderList.push("Others(Ex)", "Others(Im)")
 
-    const simpleLinks = Object.entries(integratedLinks).map(([key, value]) => {
-        const [source, target] = key.split("%");
-        return {source:source, target:target, value:value };
-    });
-
-    let nodeIds = new Set();
-    simpleLinks.forEach(d => {
-        nodeIds.add(d.source);
-        nodeIds.add(d.target);
-        });
-    let selectedNodes = Array.from(nodeIds).map(d => ({id: d}));
-
-    
     //deleting old container
     let container = document.getElementById("sankeyContainer");
     container.innerHTML = ""; 
 
+    
 
-    let sankeyDiagrm = SankeyChart({nodes: selectedNodes, links: simpleLinks}, {
+
+    const sankeyDiagrm = SankeyChart({nodes: nodes, links: links}, {
         nodeLabel: d => d.id.replace("(Ex)", "").replace("(Im)", ""),
         nodeSort: (a, b) => {
-            const A_Others = a.id === "Others(Ex)" || a.id === "Others(Im)" ;
-            const B_Others = b.id === "Others(Ex)" || b.id === "Others(Im)" ;
-
-            if (A_Others && !B_Others) return 1;
-            if (!A_Others && B_Others) return -1;
-
-            return d3.ascending(a.id, b.id);
+            return orderList.indexOf(a.id) - orderList.indexOf(b.id);
             },
         // AI I asked ChatGPT "Is there a way to color nodes in the SankeyChart function by their id while also assigning a specific color to particular vlues?" and I got an idea to use ... to map nodeGroup, nodeGroups, and color correspondingly.
         nodeGroup: d => d.id.includes("Others") ? "Others": d.id,
-        nodeGroups: ["Others", ...selectedNodes.map(d => d.id).filter(id => !id.includes("Others"))],
+        nodeGroups: ["Others", ...nodes.map(d => d.id).filter(id => !id.includes("Others"))],
         colors: ["black", ...d3.schemeTableau10],
         width: 1200,
         height: 800
@@ -111,3 +113,27 @@ function updateChart() {
 
 // Updating by pressing a button
 document.getElementById("updateButton").addEventListener("click", updateChart);
+
+function runexample(exyear, exproduct){
+    const year = document.getElementById("selectyear");
+    const product = document.getElementById("selectproduct");
+    const topN = document.getElementById("selecttopN");
+
+    year.value = exyear;
+    product.value = exproduct
+    topN.value = 5
+
+    updateChart()
+}
+
+document.getElementById("example1").addEventListener("click", ()=>{
+    runexample("2024", "Lithium carbonate")
+});
+
+document.getElementById("example2").addEventListener("click", ()=>{
+    runexample("2020", "Nickel")
+});
+
+document.getElementById("example3").addEventListener("click", ()=>{
+    runexample("2024", "Nickel")
+});
---
+++ src/main2.js
@@ -0,0 +1,65 @@
+function sortedcountry(selectedData, countries) {
+
+    const totalExport = {};
+    for (d of selectedData) {
+          totalExport[d.source] = (totalExport[d.source] || 0) + d.value;
+    };
+    
+    const totalImport = {};
+    for (d of selectedData) {
+        totalImport[d.target] = (totalImport[d.target] || 0) + d.value
+    };
+
+    const sortedExport = Object.entries(totalExport)
+        .sort((a, b) => b[1] - a[1])
+        .map(d => d[0]);
+    
+    const exportCountries = sortedExport.filter(country => countries.includes(country))
+
+    const sortedImport = Object.entries(totalImport)
+        .sort((a, b) => b[1] - a[1])
+        .map(d => d[0]);
+    
+    const importCountries = sortedImport.filter(country => countries.includes(country))
+    
+    return [exportCountries, importCountries]
+}
+
+function updatecountryChart() {
+    const year = document.getElementById("selectyear2").value;
+    const product = document.getElementById("selectproduct2").value;
+    const countryOption = document.getElementById("countryFilter");
+    const countries = Array.from(countryOption.selectedOptions).map(d => d.value);
+    const selected = tradeData
+        .filter(d => d.product === product && String(d.year) === year);
+
+    const {links, nodes} = createLinksandNodes(selected, countries, countries)
+
+    const [exportCountries, importCountries] = sortedcountry(selected, countries);
+
+    let orderList = exportCountries.map(d => d+"(Ex)")
+        .concat(importCountries.map(d => d + "(Im)"));
+    orderList.push("Others(Ex)", "Others(Im)")
+
+    //deleting old container
+    let container = document.getElementById("sankeyContainer2");
+    container.innerHTML = "";
+
+    const sankeyDiagrm = SankeyChart({nodes: nodes, links: links}, {
+        nodeLabel: d => d.id.replace("(Ex)", "").replace("(Im)", ""),
+        nodeSort: (a, b) => {
+            return orderList.indexOf(a.id) - orderList.indexOf(b.id);
+            },
+        // AI I asked ChatGPT "Is there a way to color nodes in the SankeyChart function by their id while also assigning a specific color to particular vlues?" and I got an idea to use ... to map nodeGroup, nodeGroups, and color correspondingly.
+        nodeGroup: d => d.id.includes("Others") ? "Others": d.id,
+        nodeGroups: ["Others", ...nodes.map(d => d.id).filter(id => !id.includes("Others"))],
+        colors: ["black", ...d3.schemeTableau10],
+        width: 1200,
+        height: 800
+    });
+
+    container.appendChild(sankeyDiagrm);
+}
+
+// Updating by pressing a button
+document.getElementById("updateButton2").addEventListener("click", updatecountryChart);
